c14c27bd608939531630f6e80f2f4bc55e7ffcf5
Tuan Cao <caomanhtuanbsb@yahoo.com>
2010-07-17 23:07:37 +0100

    Finished VCB and Testcase

---
+++ currency.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'dm-core'
+
+
+
+class Currency
+  include DataMapper::Resource
+  
+  property :id,               Serial
+  property :bank_code,        String ,  :required => true
+  property :currency_code,    Text,     :required => true
+  property :buy_in_cash,      Float
+  property :tranfer,          Float
+  property :sell_in_cash,     Float
+  property :updated_at,       DateTime
+  
+  #validating code !!!
+  
+  def to_s
+    "Currency Type: #{@currency_code} -- Buy: #{@buy_in_cash} -- Sell: #{@sell_in_cash} -- Tranfer: #{@tranfer} -- Updated: #{@updated_at}"
+  end
+end
---
+++ vcb_parser.rb
@@ -0,0 +1,98 @@
+require 'rubygems'
+require 'dm-migrations'
+require 'dm-validations'
+require 'dm-timestamps'
+require 'nokogiri'
+require 'open-uri'
+
+require 'currency'
+require 'test/unit'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/currency.sqlite3")
+DataMapper.auto_upgrade!
+
+class VCBParser 
+  XPath_Query = '//div[@id="exch-rates"]/div/table/tr[position()>1]'
+  def initialize(path)
+    @doc = Nokogiri::HTML(open(path))
+  end
+  
+  def parse_doc
+    @doc.xpath(XPath_Query).each do |row|
+      currency = parse_a_row(row)
+      if currency.save
+        puts 'Save successful'
+      else
+        puts 'Save failed'
+      end
+    end
+  end
+
+  def get_a_html_node(xpath_query)
+    @doc.xpath(xpath_query)
+  end
+  
+  def parse_a_row(a_row)
+     a_currency = Currency.new
+     a_currency.bank_code      = "VCB"
+     a_currency.currency_code  = a_row.children[0].content.downcase.lstrip.rstrip
+     a_currency.buy_in_cash    = a_row.children[2].content.lstrip.rstrip.gsub(',','').to_f
+     a_currency.sell_in_cash   = a_row.children[4].content.lstrip.rstrip.gsub(',','').to_f
+     a_currency.tranfer        = a_row.children[3].content.lstrip.rstrip.gsub(',','').to_f
+     return a_currency
+  end
+end
+
+
+class TestVCBParser < Test::Unit::TestCase
+  def test_data_sample
+    parser = VCBParser.new('data_sample/VCB.html')
+    assert_not_nil(parser, 'Could not init the parser')
+    
+    usd_currency = Currency.new
+    usd_currency.bank_code = "VCB"
+    usd_currency.currency_code = "usd"
+    usd_currency.buy_in_cash = 19080.00
+    usd_currency.sell_in_cash = 19095.00
+    usd_currency.tranfer = 19080.00 
+    
+    gbp_currency = Currency.new
+    gbp_currency.bank_code = "VCB"
+    gbp_currency.currency_code = "gbp"
+    gbp_currency.buy_in_cash = 28358.62
+    gbp_currency.sell_in_cash = 28951.38
+    gbp_currency.tranfer = 28558.53
+    get_gbp_query = '//div[@id="exch-rates"]/div/table/tr[7]'
+    gbp_node = parser.get_a_html_node(get_gbp_query)
+    result_currency = parser.parse_a_row(gbp_node)
+    assert_equal(gbp_currency, result_currency, 'GBP cannot be read correctly')
+    
+    
+    assert_not_nil(usd_currency, 'Cannot init usd_currency object')
+    
+    get_usd_query = '//div[@id="exch-rates"]/div/table/tr[last()]'
+    usd_node = parser.get_a_html_node(get_usd_query)
+    assert_not_nil(usd_node, 'Cannot get the node')
+    result_currency = parser.parse_a_row(usd_node)
+    
+    assert_equal(usd_currency, result_currency, 'USD cannot be read correctly')
+    #assert_equal(usd_currency, , 'USD cannot be read correctly')
+    
+    
+    
+    
+    
+    
+#    doc.xpath('//div[@id="exch-rates"]/div/table/tr[2]').each do |row|   
+ #   end
+  end
+  
+  #test number of row parsed
+  #test for first and two item see if it match
+  
+end
+
+VCB_URL = 'http://www.vietcombank.com.vn/ExchangeRates/'
+VCBParser.new(VCB_URL).parse_doc
+
+
