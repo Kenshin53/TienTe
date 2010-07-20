6bcdf8793b716199adce4b94374f4ed0aebc7491
Tuan Cao <caomanhtuanbsb@yahoo.com>
2010-07-20 20:38:23 +0100

    Added error message if no bankcode found, style the table

--- main.rb
+++ main.rb
@@ -6,16 +6,27 @@ require 'dm-migrations'
 require 'dm-validations'
 require 'dm-timestamps'
 require 'sass'
+require 'haml'
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/currency.sqlite3")
 
 
-get '/style.css' do
-  sass :style
-end
+# get '/style.css' do
+#   sass :style
+# end
 
-get '/' do
-  @currency_list = Currency.all    
-  haml :index
+get '/:bank_code' do |bankcode|
+  
+  bankcode.upcase!
+  a_currency = Currency.first(:bank_code => bankcode, :order => [:updated_at.desc])
+  if a_currency != nil
+    last_update = a_currency.updated_at
+    @currency_list = Currency.all(:updated_at.gte => last_update) 
+    haml :index
+  else 
+    @error_message = "Couldn't find the bank input"
+    haml  :index
+  end
+  
   
 end
